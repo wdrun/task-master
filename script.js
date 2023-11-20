02d81e4a37f7086d1e079890145b8d34edf8c805
wd/run <96586553+wdrun@users.noreply.github.com>
2023-11-21 00:44:50 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const inputBox = document.getElementById("input-box")
 const listContainer = document.getElementById("list-container");
+const options = {
+    bottom: '64px', // default: '32px'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
+    time: '0.3s', // default: '0.3s'
+    mixColor: '#fff', // default: '#fff'
+    backgroundColor: '#fff',  // default: '#fff'
+    buttonColorDark: '#100f2c',  // default: '#100f2c'
+    buttonColorLight: '#fff', // default: '#fff'
+    saveInCookies: true, // default: true,
+    label: '🌓', // default: ''
+    autoMatchOsTheme: true // default: true
+  }
 
+  const darkmode = new Darkmode(options)
+
+  darkmode.showWidget();
 function addTask() {
     if (inputBox.value == '') {
         alert("You can't have an empty task!");
